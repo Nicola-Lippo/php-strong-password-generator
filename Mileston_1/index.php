0e9998fcae354e91396368bf6d3ba3edfa53a5ba
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-06-19 15:40:06 +0200

    [UPD] chiamata GET

--- Mileston_1/index.php
+++ Mileston_1/index.php
@@ -1,3 +1,8 @@
+<?php
+//richiamo variabili del form
+$pw = $_GET['pw'];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,7 +13,25 @@
 </head>
 
 <body>
-
+    <div>
+        <h1>Strong Password Generator</h1>
+        <!--FORM-->
+        <form action="index.php" method="$_GET">
+            <!--Campi di input-->
+            <div>
+                <label for="text-area">Lunghezza caratteri</label>
+                <!-- Attributo name-->
+                <input type="text" id="text-area" name="pw">
+            </div>
+            <!--Bottone-->
+            <div>
+                <button>Cerca</button>
+            </div>
+        </form>
+        <div>
+            <div>La lunghezza scelta è <span><?php echo $pw; ?></span></div>
+        </div>
+    </div>
 </body>
 
 </html>
