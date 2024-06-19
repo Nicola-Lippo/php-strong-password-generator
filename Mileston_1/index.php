7addc25763760f4aa366670f954aa69a04935a1c
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-06-19 16:42:21 +0200

    [UPD]Milestone 1

--- Mileston_1/index.php
+++ Mileston_1/index.php
@@ -1,6 +1,26 @@
 <?php
 //richiamo variabili del form
 $pw = $_GET['pw'];
+//caratteri password(Array di elementi)
+$caratteri = [
+    'abcdefghilmnopqrstuvz',
+    'ABCDEFGHILMNOPQRSTUVZ',
+    '123456789',
+    '!?£%&/()',
+];
+//creazione pasword
+//inizializzo una variabile di partenza con stringa vuota
+$passwor = '';
+for ($i = 0; $i < $pw; $i++) {
+    //variabile associata una funzione che crea un numero randomico da 0 a 3 come gli elementi nel mio Array
+    $pesca = rand(0, 3);
+    //definisco la lunghezza dei miei elementi 
+    $last = strlen($caratteri[$pesca]) - 1;
+    //.= per concatenare le stringhe
+    $passwor .= $caratteri[$pesca][rand(0, $last)];
+}
+
+echo $passwor;
 ?>
 
 <!DOCTYPE html>
@@ -25,7 +45,7 @@ $pw = $_GET['pw'];
             </div>
             <!--Bottone-->
             <div>
-                <button>Cerca</button>
+                <button>Invia</button>
             </div>
         </form>
         <div>
